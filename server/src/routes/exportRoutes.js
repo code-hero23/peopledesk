36f7fe4d79fea9ffe73cfd8c20cfd1cd1ab28e0b
Aravind- Techie <webapp.cookscape@gmail.com>
2026-01-30 14:05:04 +0530

    30jan197

--- server/src/routes/exportRoutes.js
+++ server/src/routes/exportRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { exportWorkLogs, exportAttendance } = require('../controllers/exportController');
 const { protect, authorize } = require('../middlewares/authMiddleware');
 
-router.get('/worklogs', protect, authorize('ADMIN'), exportWorkLogs);
-router.get('/attendance', protect, authorize('ADMIN'), exportAttendance);
+router.get('/worklogs', protect, authorize('ADMIN', 'HR', 'BUSINESS_HEAD'), exportWorkLogs);
+router.get('/attendance', protect, authorize('ADMIN', 'HR', 'BUSINESS_HEAD'), exportAttendance);
 
 module.exports = router;
