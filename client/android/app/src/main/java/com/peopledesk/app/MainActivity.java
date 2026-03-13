e8c837bc17b3758180a6355e54bf7308e8b3a943
Aravind- Techie <webapp.cookscape@gmail.com>
2026-03-13 12:43:14 +0530

    Fix ReferenceError: simFilter is not defined in drill-down viewjnuyrwe h  vbhuyeyym sdvbdhhj whjxbjyhgsnkxugvu b rbs xxc7gu v jhngv mnb nmnbv hjnb hhncd x c ugeiyvbeyregvbt gh cbietf8wevjhnghztxdgvuyxbhc nhv bjjyer tuwbvkjchn8kyu6g fyfdtgvurftuytgiu

--- client/android/app/src/main/java/com/peopledesk/app/MainActivity.java
+++ client/android/app/src/main/java/com/peopledesk/app/MainActivity.java
@@ -2,15 +2,11 @@ package com.peopledesk.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.capacitorjs.plugins.backgroundrunner.BackgroundRunnerPlugin;
-import com.capacitorjs.plugins.preferences.PreferencesPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(CallLogPlugin.class);
-        registerPlugin(BackgroundRunnerPlugin.class);
-        registerPlugin(PreferencesPlugin.class);
+        this.registerPlugin(CallLogPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
