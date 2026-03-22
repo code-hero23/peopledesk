7cedb336c0e9aa0ca85a2e86cafd46170d970114
Aravind- Techie <webapp.cookscape@gmail.com>
2026-03-22 14:49:28 +0530

    fix: register BackgroundRunner plugin and service in native code

--- client/android/app/src/main/java/com/peopledesk/app/MainActivity.java
+++ client/android/app/src/main/java/com/peopledesk/app/MainActivity.java
@@ -2,11 +2,13 @@ package com.peopledesk.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.capacitorjs.plugins.backgroundrunner.BackgroundRunnerPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(CallLogPlugin.class);
+        registerPlugin(BackgroundRunnerPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
