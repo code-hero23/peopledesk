d9839ded1686e7a28fd4f258d148e0e308ea856d
Aravind- Techie <webapp.cookscape@gmail.com>
2026-03-22 11:28:19 +0530

    jaaaayfvuyasefvytfvgwaerftwegfwegfjtgbvjggggggggggggyyjhjuuuyj6gjby7b 6gb h6b tjvb rkyngki6hi6hni5gbk8uhfuyfbyif78576ygmgntbvjy,hukjkgnbuvjhbhjkygbujusecfgunyfgbwkuerbfucyeyhfgbkubg

--- client/src/components/ConfirmationModal.jsx
+++ client/src/components/ConfirmationModal.jsx
@@ -62,7 +62,7 @@ const ConfirmationModal = ({ isOpen, onClose, onConfirm, title, message, type = 
     return createPortal(
         <AnimatePresence>
             {isOpen && (
-                <div className="fixed inset-0 z-[9999] flex items-center justify-center p-4">
+                <div className="fixed inset-0 z-[11000] flex items-center justify-center p-4">
                     {/* Backdrop */}
                     <motion.div
                         initial={{ opacity: 0 }}
