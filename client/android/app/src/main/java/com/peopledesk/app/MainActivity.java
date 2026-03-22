1341760f3899932f65b04a4914fb0864a9ec963e
Aravind- Techie <webapp.cookscape@gmail.com>
2026-03-22 14:58:42 +0530

    fix: remove manual registration and add missing background permissions

--- client/android/app/src/main/java/com/peopledesk/app/MainActivity.java
+++ client/android/app/src/main/java/com/peopledesk/app/MainActivity.java
@@ -2,13 +2,11 @@ package com.peopledesk.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import io.ionic.backgroundrunner.plugin.BackgroundRunnerPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(CallLogPlugin.class);
-        registerPlugin(BackgroundRunnerPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
