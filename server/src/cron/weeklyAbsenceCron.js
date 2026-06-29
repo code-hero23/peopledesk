ded88f2942050a245c0d05a9b8825e32ee8bbfa5
Elakkiya Sakthivelu <elakkiya.sakthivelu3089@gmail.com>
2026-06-29 13:09:24 +0530

    cron testing

--- server/src/cron/weeklyAbsenceCron.js
+++ server/src/cron/weeklyAbsenceCron.js
@@ -8,11 +8,11 @@ const {
 } = require("../controllers/requestController");
 
 const initWeeklyExcessiveAbsenceCron = () => {
-    // cron.schedule("*/2 * * * *", async () => {
+    cron.schedule("*/2 * * * *", async () => {
 
-    cron.schedule(
-        "30 23 * * 0",
-        async () => {
+    // cron.schedule(
+    //     "30 23 * * 0",
+    //     async () => {
 
             console.log("Running Weekly Excessive Absence Report...");
 
