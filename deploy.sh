20677434d777e3201f5ec90882fd3a55807fd00d
Elakkiya Sakthivelu <elakkiya.sakthivelu3089@gmail.com>
2026-08-01 15:38:47 +0530

    deploy change

--- deploy.sh
+++ deploy.sh
@@ -33,7 +33,7 @@ bash ./gradlew assembleDebug
 
 # Move APK to a public web path for easy download if needed
 mkdir -p ../../server/uploads/apks/
-cp app/build/outputs/apk/debug/app-debug.apk ../../server/uploads/apks/peopledesk-latest-v1.0.3.apk
+cp app/build/outputs/apk/debug/app-debug.apk ../../server/uploads/apks/test(1)peopledesk-latest-v1.0.3.apk
 
 cd ../..
 
